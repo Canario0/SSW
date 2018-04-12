84cf5c5c2c081d1a1ca93bab7754c33c903b8ff6
sergio <sergi2312@gmail.com>
2018-04-12 12:42:48 +0200

    Script creacion BBDD añadido, falta modificar una clave ajena

---
+++ src/bbdd/scriptCreacionBBDD.sql
@@ -0,0 +1,63 @@
+
+-- -----------------------------------------------------
+-- Table "mydb"."USUARIO"
+-- -----------------------------------------------------
+CREATE TABLE "USUARIO" (
+  "nickname" VARCHAR(50) NOT NULL,
+  "email" VARCHAR(45 ),
+  "password" VARCHAR(45 ),
+  "nombre" VARCHAR(45 ),
+  "apellidos" VARCHAR(45 ),
+  "direccion" VARCHAR(100 ),
+  "fechaNacimiento" DATE,
+  "empresa" VARCHAR(45 ),
+  "telefono" INTEGER,
+  "sensorFav" INTEGER NOT NULL,
+  "sensorLike" INTEGER NOT NULL,
+  PRIMARY KEY ("nickname"));
+  
+-- -----------------------------------------------------
+-- Table "mydb"."SENSOR"
+-- -----------------------------------------------------
+CREATE TABLE "SENSOR" (
+  "id" INTEGER NOT NULL,
+  "nicknameUser" VARCHAR(50 ),
+  "nombre" VARCHAR(45 ),
+  "descripcion" VARCHAR(500 ),
+  "tipo" VARCHAR(45 ),
+  "visible" VARCHAR(45 ),
+  "latitud" VARCHAR(45 ),
+  "longitud" VARCHAR(45 ),
+  "numeroLikes" INTEGER,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY ("nicknameUser")
+  REFERENCES "USUARIO" ("nickname"));
+
+-- -----------------------------------------------------
+-- Table "mydb"."MEDICION"
+-- -----------------------------------------------------
+CREATE TABLE "MEDICION" (
+  "fechaSubida" DATE,
+  "fechaMedicion" DATE NOT NULL,
+  "valor" VARCHAR(45 ),
+  PRIMARY KEY ("fechaMedicion"));
+
+
+-- -----------------------------------------------------
+-- Table "mydb"."MEDICIONES"
+-- -----------------------------------------------------
+CREATE TABLE "MEDICIONES" (
+  "id" INTEGER NOT NULL,
+  "fecha" VARCHAR(45) NOT NULL,
+  PRIMARY KEY ("id", "fecha"),
+  FOREIGN KEY ("id")
+  REFERENCES "SENSOR" ("id"));
+
+
+
+-- ALTER TABLE "USUARIO" ADD FOREIGN KEY ("sensorFav" , "sensorLike") REFERENCES "SENSOR" ("id", "id");
+
+ 
+
+
+
