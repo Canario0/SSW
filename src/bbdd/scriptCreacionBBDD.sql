de91fdd3f81062df4c125838f8ae9aa38b08b473
Canario0 <prenero4bc@gmail.com>
2018-04-23 22:46:38 +0200

    Correción script

--- src/bbdd/scriptCreacionBBDD.sql
+++ src/bbdd/scriptCreacionBBDD.sql
@@ -1,38 +1,42 @@
+drop table USUARIO;
+drop table SENSOR;
+drop table LIKED;
+drop table MEDICION;
 
 -- -----------------------------------------------------
 -- Table "mydb"."USUARIO"
 -- -----------------------------------------------------
-CREATE TABLE "USUARIO" (
-  "nickname" VARCHAR(20),
-  "email" VARCHAR(45) NOT NULL,
-  "password" VARCHAR(10) NOT NULL,
-  "nombre" VARCHAR(10),
-  "apellidos" VARCHAR(20),
-  "direccion" VARCHAR(100),
-  "fechaNacimiento" DATE,
-  "empresa" VARCHAR(30),
-  "telefono" INTEGER,
-  PRIMARY KEY ("nickname"));
+CREATE TABLE USUARIO(
+  nickname VARCHAR(20) NOT NULL,
+  email VARCHAR(45) NOT NULL,
+  password VARCHAR(10) NOT NULL,
+  nombre VARCHAR(10),
+  apellidos VARCHAR(20),
+  direccion VARCHAR(100),
+  fechaNacimiento DATE,
+  empresa VARCHAR(30),
+  telefono INTEGER,
+  PRIMARY KEY (nickname));
   
 -- -----------------------------------------------------
 -- Table "mydb"."SENSOR"
 -- -----------------------------------------------------
-CREATE TABLE "SENSOR" (
-  "id" INTEGER NOT NULL,
-  "nombre" VARCHAR(10) NOT NULL,
-  "descripcion" VARCHAR(255),
-  "tipo" INTEGER NOT NULL,
-  "visible" BOOLEAN NOT NULL,
-  "x" DOUBLE PRECISION NOT NULL,
-  "y" DOUBLE PRECISION NOT NULL,
-  PRIMARY KEY ("id"));
+CREATE TABLE SENSOR (
+  id INTEGER NOT NULL,
+  nombre VARCHAR(10) NOT NULL,
+  descripcion VARCHAR(255),
+  tipo INTEGER NOT NULL,
+  visible BOOLEAN NOT NULL,
+  x DOUBLE PRECISION NOT NULL,
+  y DOUBLE PRECISION NOT NULL,
+  PRIMARY KEY (id));
 
 -- -----------------------------------------------------
 -- Table "mydb"."FAVORITO"
 -- -----------------------------------------------------
-CREATE TABLE "FAVORITO" (
-  "nickname" VARCHAR(20),
-  "id" INTEGER NOT NULL,
+CREATE TABLE FAVORITO (
+  nickname VARCHAR(20),
+  id INTEGER NOT NULL,
   PRIMARY KEY (nickname, id),
   FOREIGN KEY (nickname) REFERENCES USUARIO(nickname)
   FOREIGN KEY (id) REFERENCES USUARIO(id));
@@ -40,9 +44,9 @@ CREATE TABLE "FAVORITO" (
 -- -----------------------------------------------------
 -- Table "mydb"."FAVORITO"
 -- -----------------------------------------------------
-CREATE TABLE "LIKE" (
-  "nickname" VARCHAR(20),
-  "id" INTEGER NOT NULL,
+CREATE TABLE LIKED (
+  nickname VARCHAR(20),
+  id INTEGER NOT NULL,
   PRIMARY KEY (nickname, id),
   FOREIGN KEY (nickname) REFERENCES USUARIO(nickname)
   FOREIGN KEY (id) REFERENCES USUARIO(id));
@@ -50,10 +54,10 @@ CREATE TABLE "LIKE" (
 -- -----------------------------------------------------
 -- Table "mydb"."MEDICION"
 -- -----------------------------------------------------
-CREATE TABLE "MEDICION" (
-  "id" INTEGER NOT NULL,
-  "fechaSubida" DATETIME,
-  "fechaMedicion" DATE NOT NULL,
-  "valor" DOUBLE PRECISION,
-  PRIMARY KEY ("fechaSubida"),
+CREATE TABLE MEDICION (
+  id INTEGER NOT NULL,
+  fechaSubida DATETIME,
+  fechaMedicion DATE NOT NULL,
+  valor DOUBLE PRECISION,
+  PRIMARY KEY (fechaSubida),
   FOREIGN KEY (id) REFERENCES SENSOR(id));
