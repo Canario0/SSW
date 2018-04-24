931ecce78af7dc250aaf9f04a9b51429308fd1a7
Canario0 <prenero4bc@gmail.com>
2018-04-24 11:39:32 +0200

    x

--- src/bbdd/scriptCreacionBBDD.sql
+++ src/bbdd/scriptCreacionBBDD.sql
@@ -1,13 +1,18 @@
-DROP TABLE IF EXISTS SENSOR;
 DROP TABLE IF EXISTS LIKED;
 DROP TABLE IF EXISTS FAVORITO;
 DROP TABLE IF EXISTS MEDICION;
+DROP TABLE IF EXISTS SENSOR;
 DROP TABLE IF EXISTS USUARIO;
+DROP TABLE IF EXISTS liked;
+DROP TABLE IF EXISTS favorito;
+DROP TABLE IF EXISTS medicion;
+DROP TABLE IF EXISTS sensor;
+DROP TABLE IF EXISTS usuario;
 
 -- -----------------------------------------------------
--- Table "mydb"."USUARIO"
+-- Table "mydb"."usuario"
 -- -----------------------------------------------------
-CREATE TABLE USUARIO(
+CREATE TABLE usuario(
   nickname VARCHAR(20) NOT NULL,
   email VARCHAR(45),
   password VARCHAR(10) NOT NULL,
@@ -20,9 +25,9 @@ CREATE TABLE USUARIO(
   PRIMARY KEY (nickname));
   
 -- -----------------------------------------------------
--- Table "mydb"."SENSOR"
+-- Table "mydb"."sensor"
 -- -----------------------------------------------------
-CREATE TABLE SENSOR (
+CREATE TABLE sensor (
   id INTEGER NOT NULL,
   nombre VARCHAR(10) NOT NULL,
   descripcion VARCHAR(255),
@@ -33,32 +38,32 @@ CREATE TABLE SENSOR (
   PRIMARY KEY (id));
 
 -- -----------------------------------------------------
--- Table "mydb"."FAVORITO"
+-- Table "mydb"."favorito"
 -- -----------------------------------------------------
-CREATE TABLE FAVORITO (
+CREATE TABLE favorito (
   nickname VARCHAR(20),
   id INTEGER NOT NULL,
   PRIMARY KEY (nickname, id),
-  FOREIGN KEY (nickname) REFERENCES USUARIO(nickname),
-  FOREIGN KEY (id) REFERENCES SENSOR(id));
+  FOREIGN KEY (nickname) REFERENCES usuario(nickname),
+  FOREIGN KEY (id) REFERENCES sensor(id));
 
 -- -----------------------------------------------------
--- Table "mydb"."FAVORITO"
+-- Table "mydb"."favorito"
 -- -----------------------------------------------------
-CREATE TABLE LIKED (
+CREATE TABLE liked (
   nickname VARCHAR(20),
   id INTEGER NOT NULL,
   PRIMARY KEY (nickname, id),
-  FOREIGN KEY (nickname) REFERENCES USUARIO(nickname),
-  FOREIGN KEY (id) REFERENCES SENSOR(id));
+  FOREIGN KEY (nickname) REFERENCES usuario(nickname),
+  FOREIGN KEY (id) REFERENCES sensor(id));
 
 -- -----------------------------------------------------
--- Table "mydb"."MEDICION"
+-- Table "mydb"."medicion"
 -- -----------------------------------------------------
-CREATE TABLE MEDICION (
+CREATE TABLE medicion (
   id INTEGER NOT NULL,
   fechaSubida DATETIME,
   fechaMedicion DATE NOT NULL,
   valor DOUBLE PRECISION,
   PRIMARY KEY (fechaSubida),
-  FOREIGN KEY (id) REFERENCES SENSOR(id));
+  FOREIGN KEY (id) REFERENCES sensor(id));
