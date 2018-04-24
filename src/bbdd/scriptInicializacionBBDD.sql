82b3e3146bb2f1708037a511ebd0cc53f26e3612
Álvaro Berruezo <a.berruezo97@gmail.com>
2018-04-24 17:38:09 +0200

    Inicialización bbdd

---
+++ src/bbdd/scriptInicializacionBBDD.sql
@@ -0,0 +1,11 @@
+insert into usuario values ('Pipo', 'pipo@dog.com', 'opipopi', 'Pipo', 'Canino', null, str_to_date('24-04-2018', '%d-%m-%Y'), null, null);
+insert into usuario values ('El Retrasito', 'el@retrasito.com', 'retrasito123', 'El', 'Retrasito', null, str_to_date('21-09-2017', '%d-%m-%Y'), null, null);
+insert into usuario values ('Antimateria', 'sergio.esteban@alumnos.uva.es', '1234567890', 'Sergio', 'Esteban', 'Calle Ciudad de la Habana 23', str_to_date('01-01-1997', '%d-%m-%Y'), null, 719537216);
+insert into usuario values ('Canario', 'pablo.renero@alumnos.uva.es', 'c4n4r10', 'Pablo', 'Renero', 'Calle Ciudad de la Habana 24', str_to_date('31-12-1997', '%d-%m-%Y'), null, 689340123);
+insert into usuario values ('Smokt', 'alejandro.martinez@alumnos.uva.es', 'contraseña', 'Alejandro', 'Martinez', 'Plaza de España 9', str_to_date('29-02-1997', '%d-%m-%Y'), null, 681234517);
+insert into usuario values ('0ravla', 'alvaro.berruezo@alumnos.uva.es', '1234', 'Alvaro', 'Berruezo', 'Calle Me Falta un Tornillo 21', str_to_date('19-09-1997', '%d-%m-%Y'), null, 684932185);
+insert into usuario values ('PeterGriffin', 'peter.griffin@alumnos.uva.es', 'grifffffin', 'Peter', 'Griffin', 'Calle Spooner 31', str_to_date('12-08-1975', '%d-%m-%Y'), 'Padre de Familia', 693728612);
+insert into usuario values ('HomerJSimpson', 'homer.j@simpson.com', 'homero', 'Homer', 'Simpson', 'Evergreen Terrace 742', str_to_date('22-02-1980', '%d-%m-%Y'), 'Los Simpson', 649334821);
+insert into usuario values ('JVegas', 'vegas@apple.com', 'aloha', 'J', 'Vegas', null, str_to_date('11-02-1973', '%d-%m-%Y'), 'UVa', 983123456);
+insert into usuario values ('CVaca', 'cesar@infor.uva.es', 'n00b', 'Cesar', 'Vaca', null, str_to_date('01-07-1979', '%d-%m-%Y'), 'UVa', 983321321321);
+insert into usuario values ('Benja', 'ben@ja.min', 'minjaben', 'Benjamin', 'Sahelices', 'Campus Universitario Miguel Delibes', str_to_date('24-06-1971', '%d-%m-%Y'), 'UVa', 983123123);
