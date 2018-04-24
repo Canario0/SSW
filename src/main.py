41a08de014c3959a1130dc643369bdb101d538a7
sergio <sergi2312@gmail.com>
2018-04-24 11:07:17 +0200

    Cambios main.py y BBDD

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 #coding: utf-8
 from flask import Flask, render_template, request, redirect, url_for
+from peewee import Model, MySQLDatabase,BooleanField, CharField, IntegerField, DateField, DateTimeField, ForeignKeyField, CompositeKey, DoubleField
+import configparser
+import datetime
 
 
 app = Flask(__name__)
@@ -82,17 +85,92 @@ def fav(user):
 def registrar_sensor(user):
     return render_template('registrar_sensor.html', user = user)
 
-
-
-
-
 @app.route("/HolaMundo")
 def hello():
     return "Hello World3!"
 
-# Es llamado anstes de terminar la petición
-# @app.teardown_appcontext
-# def close_db(error):
-#     """Closes the database again at the end of the request."""
-#     if hasattr(g, 'sqlite_db'):
-#         g.sqlite_db.close()
+#----------------------------------------------------------------------
+
+conf = configparser.ConfigParser()
+conf.read('config.txt')
+
+db = MySQLDatabase(conf['DataBase']['name'], user=conf['DataBase']['user'], password=conf['DataBase']['password'], host=conf['DataBase']['host'], port=int(conf['DataBase']['port']))
+
+
+class Usuario(Model):
+    nickname = CharField(max_length=20, primary_key=True)
+    email = CharField(max_length=45,null=True)
+    password = CharField(max_length=10, null=False)
+    nombre = CharField(max_length=10, null=True)
+    apellidos = CharField(max_length=20, null=True)
+    direccion = CharField(max_length=100, null=True)
+    nacimiento = DateField(null=True)
+    empresa = CharField(max_length=30, null=True)
+    telefono = IntegerField(null=True)
+
+    class Meta:
+        database = db
+
+
+class Sensor(Model):
+    id = IntegerField(primary_key=True)
+    nombre = CharField(max_length=10, null=False)
+    descripcion = CharField(null=True)
+    tipo = IntegerField(null=False)
+    visible = BooleanField(null=False, default=True)
+    x = DoubleField(null=False)
+    y = DoubleField(null=False)
+
+    class Meta:
+        database = db
+
+
+class Favorito(Model):
+    nickname = ForeignKeyField(Usuario)
+    id = ForeignKeyField(Sensor)
+
+    class Meta:
+        database = db
+        primary_key = CompositeKey('nickname', 'tag')
+
+
+class Like(Model):
+    nickname = ForeignKeyField(Usuario)
+    id = ForeignKeyField(Sensor)
+
+    class Meta:
+        database = db
+        primary_key = CompositeKey('nickname', 'tag')
+
+
+class Medicion(Model):
+    id = ForeignKeyField(Sensor, backref='mediciones')
+    # este valor se autogenera
+    fechaSubida = DateTimeField(
+        default=datetime.datetime.now, primary_key=True)
+    fechaMedicion = DateField(null=False)
+    valor = DoubleField(null=False)
+
+    class Meta:
+        database = db
+
+
+def create_Usuario(nickname, password):
+    with db.atomic():
+        Usuario.create(nickname=nickname, password=password)
+
+
+#db.connect()
+@app.before_request
+def before_request():
+    db.connect()
+
+@app.after_request
+def after_request(response):
+    db.close()
+    return response
+
+create_Usuario('pipo', '1235aaaa')
+user = list(Usuario.select().where(Usuario.nickname=='pipo').dicts())
+print(user)
+
