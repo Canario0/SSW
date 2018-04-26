b6d7931d9979159c5e6a0872cda5ab3e0dc3bf3a
sergio <sergi2312@gmail.com>
2018-04-27 01:01:47 +0200

    Script bbdd refactorizado y bonito

--- src/bbdd/scriptCreacionBBDD.sql
+++ src/bbdd/scriptCreacionBBDD.sql
@@ -1,3 +1,6 @@
+-- -----------------------------------------------------
+-- If tables already exists, delete them             
+-- -----------------------------------------------------
 DROP TABLE IF EXISTS LIKED;
 DROP TABLE IF EXISTS FAVORITO;
 DROP TABLE IF EXISTS MEDICION;
@@ -75,26 +78,138 @@ CREATE TABLE medicion (
 -- ----------------------------------------------------
 -- BBDD inizialiation
 -- ----------------------------------------------------
-insert into usuario values ('pipo', 'pipo@dog.com', 'opipopi', 'Pipo', 'Canino', null, null, str_to_date('24-04-2018', '%d-%m-%Y'), null, null);
-insert into usuario values ('El Retrasito', 'el@retrasito.com', 'retrasito1', 'El', 'Retrasito',  null, null, str_to_date('21-09-2017', '%d-%m-%Y'), null, null);
-insert into usuario values ('Antimateria', 'sergio.esteban@alumnos.uva.es', '1234567890', 'Sergio', 'Esteban', null,'Calle Ciudad de la Habana 23', str_to_date('01-01-1997', '%d-%m-%Y'), null, 719537216);
-insert into usuario values ('Canario', 'pablo.renero@alumnos.uva.es', 'c4n4r10', 'Pablo', 'Renero', null, 'Calle Ciudad de la Habana 24', str_to_date('31-12-1997', '%d-%m-%Y'), null, 689340123);
-insert into usuario values ('Smokt', 'alejandro.martinez@alumnos.uva.es', 'contraseña', 'Alejandro', 'Martinez', null, 'Plaza de España 9', str_to_date('01-02-1997', '%d-%m-%Y'), null, 681234517);
-insert into usuario values ('0ravla', 'alvaro.berruezo@alumnos.uva.es', '1234', 'Alvaro', 'Berruezo', null, 'Calle Me Falta un Tornillo 21', str_to_date('19-09-1997', '%d-%m-%Y'), null, 684932185);
-insert into usuario values ('PeterGriffin', 'peter.griffin@alumnos.uva.es', 'grifffffin', 'Peter', 'Griffin', null, 'Calle Spooner 31', str_to_date('12-08-1975', '%d-%m-%Y'), 'Padre de Familia', 693728612);
-insert into usuario values ('HomerJSimpson', 'homer.j@simpson.com', 'homero', 'Homer', 'Simpson', null, 'Evergreen Terrace 742', str_to_date('22-02-1980', '%d-%m-%Y'), 'Los Simpson', 649334821);
-insert into usuario values ('JVegas', 'vegas@apple.com', 'aloha', 'J', 'Vegas', null, null, str_to_date('11-02-1973', '%d-%m-%Y'), 'UVa', 983123456);
-insert into usuario values ('CVaca', 'cesar@infor.uva.es', 'n00b', 'Cesar', 'Vaca', null, null, str_to_date('01-07-1979', '%d-%m-%Y'), 'UVa', 983123456);
-insert into usuario values ('Benja', 'ben@ja.min', 'minjaben', 'Benjamin', 'Sahelices', null, 'Campus Universitario Miguel Delibes', str_to_date('24-06-1971', '%d-%m-%Y'), 'UVa', 983123123);
 
-insert into sensor values (1, 'pipo', "HC05", "sesor de temperatura", 2, true, 236.25, 2547.36);
-insert into sensor values (2, 'pipo', "HC05-v42", "sensor del amor", 3, false, 230.7, 200.1);
-insert into sensor values (3, 'Smokt', "HC05-v14", "sensor de feromonas", 3, true, 203.7, 201.1);
-insert into medicion values (1, Date_format(now(),'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.15); 
-insert into medicion values (1, Date_format(now()+1,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.11); 
-insert into medicion values (1, Date_format(now()+2,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.12); 
-insert into medicion values (1, Date_format(now()+3,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.13); 
-insert into medicion values (1, Date_format(now()+4,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.14); 
-insert into medicion values (1, Date_format(now()+5,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.16); 
-insert into medicion values (1, Date_format(now()+6,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.17); 
-insert into medicion values (1, Date_format(now()+7,'%Y-%m-%d %h:%i:%s'), Date_format(now(), '%Y-%m-%d'), 20.18); 
+
+-- ----------------------------------------------------
+-- Values for some users
+-- ----------------------------------------------------
+insert into usuario values (
+    'sergio',
+    'sergio.esteban@alumnos.uva.es',
+    'sergio123456789',
+    'Sergio',
+    'Esteban',
+    'Pellejero',
+    'Valladolid - Parquesol',
+    str_to_date('24-04-2018', '%d-%m-%Y'),
+    'UVa',
+    365412879);
+
+insert into usuario values (
+    'pablo',
+    'pablo.renero@alumnos.uva.es',
+    'pablo123456789',
+    'Pablo',
+    'Renero',
+    'Balganon',
+    'Valladolid - Parquesol',
+    str_to_date('24-04-2018', '%d-%m-%Y'),
+    'UVa',
+    965874123);
+
+insert into usuario values (
+    'alvaro',
+    'alvaro.berruezo@alumnos.uva.es',
+    'alvaro123456789',
+    'Alvaro',
+    'Berruezo',
+    'Franco',
+    'Valladolid - Centro',
+    str_to_date('24-04-2018', '%d-%m-%Y'),
+    'UVa',
+    548632158);
+
+insert into usuario values (
+    'alejandro',
+    'alejandro.martinez@alumnos.uva.es',
+    'alejandro123456789',
+    'Alejandro',
+    'Martinez',
+    'Andres',
+    'Valladolid - Parquesol',
+    str_to_date('24-04-2018', '%d-%m-%Y'),
+    'UVa',
+    231254687);
+
+-- ----------------------------------------------------
+-- Values for some sensors
+-- ----------------------------------------------------
+insert into sensor values (1, 
+    'sergio',
+    'HFC05', 
+    'sensor temperatura',
+    2,
+    true,
+    236.25,
+    2547.36);
+
+insert into sensor values (2, 
+    'sergio',
+    'HFC05', 
+    'sensor temperatura',
+    2,
+    true,
+    356.25,
+    587.36);
+
+insert into sensor values (3, 
+    'alejandro',
+    'JUI87', 
+    'sensor humedad',
+    3,
+    true,
+    937.22,
+    243.37);
+
+insert into sensor values (4, 
+    'alvaro',
+    'AFJ56', 
+    'sensor ruido',
+    4,
+    true,
+    36.15,
+    47.26);
+
+
+-- ----------------------------------------------------
+-- Values for some meditions
+-- ----------------------------------------------------
+insert into medicion values (1,
+    Date_format(now(), '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.11); 
+
+insert into medicion values (1,
+    Date_format(now()+1, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.12); 
+
+insert into medicion values (1,
+    Date_format(now()+2, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.13); 
+
+insert into medicion values (1,
+    Date_format(now()+3, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.14); 
+
+insert into medicion values (1,
+    Date_format(now()+4, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.15); 
+
+insert into medicion values (1,
+    Date_format(now()+5, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.16); 
+
+insert into medicion values (1,
+    Date_format(now()+6, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.17); 
+
+insert into medicion values (1,
+    Date_format(now()+7, '%Y-%m-%d %h:%i:%s'),
+    Date_format(now(), '%Y-%m-%d'), 
+    20.18); 
