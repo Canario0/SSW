7c1fe0727debed86eabb9cea250d05494c03470c
Canario0 <prenero4bc@gmail.com>
2018-05-10 10:44:46 +0200

    Bug fix

--- src/static/js/LoginValidation.js
+++ src/static/js/LoginValidation.js
@@ -1,5 +1,5 @@
 function validateForm() {
-    var x = document.forms["myForm"]["fname"].value;
+    var x = document.forms["myForm"]["nick-name"].value;
     if (x == "") {
         alert("Name must be filled out");
         return false;
