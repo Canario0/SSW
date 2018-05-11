1243f6355e780d5f0e8be645df8aa8453407da12
Canario0 <prenero4bc@gmail.com>
2018-05-11 20:07:34 +0200

    Os mato

--- src/app.py
+++ src/app.py
@@ -135,7 +135,7 @@ def logged_index(user):
     else:
         if current_user.is_authenticated:
             print("holi2")
-            return redirect(url_for('logged_index'), user=user)
+            return redirect(url_for('logged_index'))
         else:
             print("holi3")
             return redirect(url_for('index'))
@@ -185,7 +185,7 @@ def registrar_sensor(user):
         desc = request.form['descripcion']
         x = request.form['lat']
         y = request.form['long']
-        createSensor(10, nombre, descripcion, 1, True, float(x), float(y))
+        create_Sensor(10, nombre, desc, 1, True, float(x), float(y))
         return redirect(url_for('logged_index', user = current_user.nickname))
 
 
