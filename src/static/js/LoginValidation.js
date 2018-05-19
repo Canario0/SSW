e753bc42a8ff65d8d81ec8c55b3df33e00dd2eba
Canario0 <prenero4bc@gmail.com>
2018-05-20 00:29:12 +0200

    Bug al incluir en js

--- src/static/js/LoginValidation.js
+++ src/static/js/LoginValidation.js
@@ -1,11 +1,13 @@
 function validateForm() {
     var x = document.forms["myForm"]["nick-name"].value;
+
     if (x == "") {
         alert("Nombre inválido");
         return false;
     }
 
     var x = document.forms["myForm"]["contraseña"].value;
+
     if (x == "") {
         alert("Contraseña incorrecta");
         return false;
