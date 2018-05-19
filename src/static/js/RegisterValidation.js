cf7f68fb19eb45d8cf63c2b02eea4d8123a7909e
Canario0 <prenero4bc@gmail.com>
2018-05-19 13:41:11 +0200

    Revisando la comporbación

--- src/static/js/RegisterValidation.js
+++ src/static/js/RegisterValidation.js
@@ -1,18 +1,18 @@
 function validaForm() {
     var x = document.forms["myForm"]["nick-name"].value;
-    if(x == "") {
+    if(x == " ") {
         alert("Name must be filled out");
         return false;
     }
 
     var x = document.forms["myForm"]["contraseña"].value;
-    if(x == "") {
+    if(x == " ") {
         alert("Password must be filled out");
         return false;
     }
 
     var x = document.forms["myForm"]["recontraseña"].valaue;
-    if(x == "") {
+    if(x == " ") {
         alert("Every field must be filled out");
         return false;
     }
