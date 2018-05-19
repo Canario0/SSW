104787c75dfe5503c16a09a38674e88502587b55
Canario0 <prenero4bc@gmail.com>
2018-05-19 13:58:56 +0200

    Registrar yaa funciona

--- src/static/js/RegisterValidation.js
+++ src/static/js/RegisterValidation.js
@@ -1,18 +1,20 @@
 function validaForm() {
     var x = document.forms["myForm"]["nick-name"].value;
-    console.log(x);
+    //console.log(x);
     if(x == "") {
         alert("Nombre inválido");
         return false
     }
 
     var y = document.forms["myForm"]["contraseña"].value;
+
     if(y == "") {
         alert("Introduzca una contraseña");
         return false;
     }
 
     var x = document.forms["myForm"]["recontraseña"].value;
+    
     if(x != y) {
         alert("La contraseña no coincide");
         return false;
