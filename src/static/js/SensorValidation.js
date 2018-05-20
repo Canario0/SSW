de3c41c3e574c188b87556b566734b79076397a1
Canario0 <prenero4bc@gmail.com>
2018-05-20 17:12:17 +0200

    Probando NAN

--- src/static/js/SensorValidation.js
+++ src/static/js/SensorValidation.js
@@ -7,8 +7,8 @@ function validateSensor() {
     }
 
 
-    var x = document.forms["myForm"]["lat"].value;
-    if (typeof (x) != 'number') {
+    var x = Number(document.forms["myForm"]["lat"].value);
+    if (x == NaN) {
         alert("La latitud debe ser un valor entero");
         return false;
     }
@@ -17,8 +17,8 @@ function validateSensor() {
         return false;
     }
 
-    var x = document.forms["myForm"]["long"].value;
-    if (typeof (x) != 'number') {
+    var x = Number(document.forms["myForm"]["long"].value);
+    if (x == NaN) {
         alert("La longitud debe ser un valor entero");
         return false;
     }
