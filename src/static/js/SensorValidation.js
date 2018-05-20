dc721ce3413c1a2bb9942782c897239c260c87ee
Canario0 <prenero4bc@gmail.com>
2018-05-20 17:30:41 +0200

    Validación via html y js

--- src/static/js/SensorValidation.js
+++ src/static/js/SensorValidation.js
@@ -1,8 +1,17 @@
 function validateSensor() {
     var name = document.forms["myForm"]["nombre"].value;
-    console.log(name);
     if (name == "") {
         alert("El campo ''Nombre Sensor'' es obligatorio");
+        return false;
+    }
+    var x = document.forms["myForm"]["lat"].value;
+    if (x == "") {
+        alert("Coordenadas incorrectas");
+        return false;
+    }
+    var x = document.forms["myForm"]["long"].value;
+    if (x == "") {
+        alert("Coordenadas incorrectas");
         return false;
     }
 }
