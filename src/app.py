6de984be67f9aecac253e93f2e6a61eb42da1132
Álvaro Berruezo <a.berruezo97@gmail.com>
2018-05-21 19:38:52 +0200

    Redirect

--- src/app.py
+++ src/app.py
@@ -19,7 +19,7 @@ tipos_sensor={"Temperatura":1, "Humedad":2, "Iluminación":3, "Contaminación":4
 @app.route("/default_img")
 def default_img():
     print("get img")
-    return url_for('static', filename='img/users/default.png')
+    return redirect(url_for('static', filename='img/users/default.png'))
 
 @app.route("/")
 @app.route("/index")
