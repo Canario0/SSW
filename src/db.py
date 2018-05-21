fd52e545a3e616fc8ae3e8b50dff3dcb13376539
Canario0 <prenero4bc@gmail.com>
2018-05-21 22:52:23 +0200

    Último retoque antes del pr

--- src/db.py
+++ src/db.py
@@ -109,7 +109,7 @@ def update_Sensor(sensor):
 
 def delete_Sensor(id):
     with db.atomic():
-        print (Sensor.delete().where(Sensor.id == id).execute())
+        Sensor.delete().where(Sensor.id == id).execute()
 
 #----------------------------------------------------------------------------
 
